57c78cad5e9bc7556178ba3fee800153013de7f2
Robin Nair <robinnair925@gmail.com>
2026-01-18 11:37:20 +0530

    Added the apple spawning mechanic and the Growth mechanic

--- script.js
+++ script.js
@@ -1,68 +1,91 @@
-const canvas=document.getElementById('snake')
-const ctx=canvas.getContext('2d');
+const canvas = document.getElementById('snake')
+const ctx = canvas.getContext('2d');
 
-const ROWS=20;
-const COLS=20;
+const ROWS = 20;
+const COLS = 20;
 
-const moveInterval=300;
-let lastTime=0;
+const moveInterval = 100;
+let lastTime = 0;
 
-canvas.width=COLS*20;
-canvas.height=ROWS*20;
+let apple = { x: 0, y: 0 };
 
-ctx.scale(20,20);
-let direction={x:0,y:1};
-document.addEventListener('keydown', event=>{
-    if(event.key=='ArrowDown'){
-        direction={x:0,y:1};
+canvas.width = COLS * 20;
+canvas.height = ROWS * 20;
+
+ctx.scale(20, 20);
+let direction = { x: 0, y: 1 };
+document.addEventListener('keydown', event => {
+    if (event.key == 'ArrowDown') {
+        direction = { x: 0, y: 1 };
     }
-    if(event.key=='ArrowUp'){
-        direction={x:0,y:-1};
+    if (event.key == 'ArrowUp') {
+        direction = { x: 0, y: -1 };
     }
-    if(event.key=='ArrowLeft'){
-        direction={x:-1,y:0};
+    if (event.key == 'ArrowLeft') {
+        direction = { x: -1, y: 0 };
     }
-    if(event.key=='ArrowRight'){
-        direction={x:1,y:0};
+    if (event.key == 'ArrowRight') {
+        direction = { x: 1, y: 0 };
     }
 });
 
-function createSnake(){
-    return{
-        body:[{x:10,y:0},
-              {x:10,y:1},
-              {x:10,y:2}],
-        direction:"DOWN"
+function createSnake() {
+    return {
+        body: [{ x: 10, y: 0 },
+        { x: 10, y: 1 },
+        { x: 10, y: 2 }],
+        direction: "DOWN"
     };
 }
 
-const Snake=createSnake();
+const Snake = createSnake();
 
-function drawSnake()
-{
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle="lightgreen";
-    for(let segment of Snake.body){
-        ctx.fillRect(segment.x,segment.y,1,1);
+function spawnapple() {
+    while (true) {
+        let x = Math.floor(Math.random() * COLS);
+        let y = Math.floor(Math.random() * ROWS);
+        const onsnake = Snake.body.some(seg => seg.x === x && seg.y === y);
+        if (!onsnake) {
+            apple = { x: x, y: y };
+            break;
+        }
     }
 }
 
-function movesnake(dir){
-    const head=Snake.body[0];
-    const newhead={
-        x:(head.x+dir.x+COLS)%COLS,
-        y:(head.y+dir.y+ROWS)%ROWS
+function drawSnake() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "lightgreen";
+    for (let segment of Snake.body) {
+        ctx.fillRect(segment.x, segment.y, 1, 1);
     }
-    Snake.body.unshift(newhead);
-    Snake.body.pop();
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = "red"
+    ctx.fillRect(apple.x, apple.y, 1, 1)
+}
+
+function movesnake(dir) {
+    const head = Snake.body[0];
+    const newhead = {
+        x: (head.x + dir.x + COLS) % COLS,
+        y: (head.y + dir.y + ROWS) % ROWS
+    }
+    if(newhead.x===apple.x&&newhead.y===apple.y)
+    {
+        spawnapple();
+        Snake.body.unshift(newhead);
+    }
+    else
+    {
+        Snake.body.unshift(newhead);
+        Snake.body.pop();
+    }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawSnake();
 }
 
-function gameLoop(time=0){
-    if(time-lastTime>moveInterval){
+function gameLoop(time = 0) {
+    if (time - lastTime > moveInterval) {
         movesnake(direction);
-        lastTime=time;
+        lastTime = time;
     }
     requestAnimationFrame(gameLoop);
 }
